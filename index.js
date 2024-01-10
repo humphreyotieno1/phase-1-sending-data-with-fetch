6216aa465b7acf28f9319f06f892216f4c51e848
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-10 23:12:06 +0300

    Fetch function

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const formData = {
+        name: userName,
+        email: userEmail,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then((response) => response.json())
+        .then((data) => {
+            document.body.innerHTML += `<p>ID: ${data.id}</p>`;
+        })
+        .catch((error) => {
+            document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+        });
+};
